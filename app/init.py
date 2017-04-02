d6aa554ff727e0e06d229ea1b658e82deb15afee
shippo <shipporun@gmail.com>
2017-04-03 00:50:07 +0800

    Update init.py

--- app/init.py
+++ app/init.py
@@ -1,12 +1,14 @@
 # -*- coding:utf8 -*-
 import time
+import hashlib
+import xml.etree.ElementTree as ET
 from flask import Flask
 from flask import request
 from flask import make_response
-import hashlib
-import xml.etree.ElementTree as ET
+from forcast import fetchWeather, sortData
 
 app = Flask(__name__)
+listH = []
 
 @app.route('/', methods=['POST', 'GET'])
 def wechat():
@@ -29,6 +31,20 @@ def wechat():
 			<Content><![CDATA[%s]]></Content> \
 			<FuncFlag>0</FuncFlag></xml>'  #
 		if msgtype == 'text':
+			if content == 'help':
+				content = '输入 城市名，获取当前天气 \
+						输入 history，获取查询记录\
+						输入 help，获取帮助文档'
+			elif content == 'history'
+				content = set(listH)
+			else:
+				data = fetchWeather(content, 1)
+				if 'status' in data:
+					content = 'No result. Need help?'
+				else:
+					a = sortData(data, key, 1)
+					content = a[0]
+					listH.append(a[0])
 			o = make_response(reply % (user, server, str(int(time.time())), content))		
 			o.content_type = 'application/xml'   #
 			return o
