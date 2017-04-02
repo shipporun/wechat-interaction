f947f9595968353d920a81d494bcc82786f03c67
shippo <shipporun@gmail.com>
2017-04-02 23:34:44 +0800

    Update init.py

--- app/init.py
+++ app/init.py
@@ -1,7 +1,10 @@
 # -*- coding:utf8 -*-
+import time
 from flask import Flask
 from flask import request
+from flask import make_response
 import hashlib
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
 
@@ -9,12 +12,25 @@ app = Flask(__name__)
 def wechat_auth():
 	if request.method == 'GET':
 		if len(request.args) > 3:
-			echostr = request.args['echostr']
+			echostr = request.args['echostr']	#
 			return echostr
 		else:
 			return 'Wanna get something?'
 	else:
-		return 'say hi to captain Butler'
+		i = ET.fromstring(request.data)		#
+		server = i.find('ToUserName').text
+		user = i.find('FromUserName').text
+		content = i.find('Content').text
+		reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName> \
+				<FromUserName><![CDATA[%s]]></FromUserName> \
+				<CreateTime><![CDATA[%s]]></CreateTime> \
+				<MsgType><![CDATA[text]]></MsgType> \
+				<Content><![CDATA[%s]]></Content> \
+				<FuncFlag>0</FuncFlag></xml>'  #
+		o = make_response(reply % (user, server, str(int(time.time()))), content)
+		o.content_type = 'application/xml'   #
+		return o
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=80) 
